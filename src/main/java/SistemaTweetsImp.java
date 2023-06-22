f7d8a55978d5956637fa438e3357e28c5821cbdd
ggiao2001 <giaomatai@gmail.com>
2023-06-22 19:49:38 -0300

    especificaciones de funciones en comentarios

--- src/main/java/SistemaTweetsImp.java
+++ src/main/java/SistemaTweetsImp.java
@@ -17,6 +17,11 @@ public class SistemaTweetsImp implements SistemaTweets{
     MyLinkedListImp<HashTag> hashtags = CSV.hashTagLinkedList;
     MyLinkedListImp<String> pilotos = CSV.driversLinkedList;
 
+    //Listar los 10 pilotos activos en la temporada 2023 más mencionados en los tweets
+    //en un mes (este mes será ingresado como 2 parámetros separados, mes y año, por
+    //consola). Este listado debe incluir el nombre de los pilotos y la cantidad de
+    //ocurrencias para cada uno de manera ordenada. Se espera que esta operación sea
+    //de orden n en notación Big O.
     @Override
     public MyQueueImp<String> pilotosMasMencionadosMes(int month, int year) throws FullHeapException, EmptyTreeException {
 
@@ -26,10 +31,10 @@ public class SistemaTweetsImp implements SistemaTweets{
         LocalDate fecha1 = LocalDate.of(year, month, 1);
         LocalDate fecha2 = LocalDate.of(year, month+1, 1);
 
-        for (int j = 0; j < pilotos.size(); j++) {
+        for (int j = 0; j < pilotos.size(); j++) { //cantidad de pilotos fija --> orden 1
             String nombreP = pilotos.get(j);
             Piloto p = new Piloto(nombreP);
-            for (int i = 0; i < listaTweets.size(); i++) {
+            for (int i = 0; i < listaTweets.size(); i++) { //recorrer todos los tweets --> orden n
                 Tweet t = listaTweets.get(i);
                 if (t.getDate().toLocalDate().compareTo(fecha1) >= 0 && t.getDate().toLocalDate().compareTo(fecha2) < 0) {
                     if (t.getContent().contains(nombreP)) {
@@ -45,11 +50,18 @@ public class SistemaTweetsImp implements SistemaTweets{
         return topPilotos;
     }
 
+    //listar los 15 usuarios con mas tweets incluyendo  nombre de usuario, la
+    //cantidad de tweets, y si el usuario está verificado o no, ordenado por cantidad de
+    //tweets en orden descendente. Se espera que esta operación sea de orden n en
+    //notación Big O.
     @Override
     public MyQueue<User> usuariosMasTweets() {
+
         return null;
     }
 
+    //Cantidad de hashtags distintos para un día dado. El día será ingresado en el formato
+    //YYYY-MM-DD.
     @Override
     public int cantidadHashtags(LocalDate dia) {
         MyHashTableImp<Long, HashTag> hashtagsDia = new MyHashTableImp();
@@ -69,6 +81,8 @@ public class SistemaTweetsImp implements SistemaTweets{
         return contador;
     }
 
+    //Hashtag más usado para un día dado, sin tener en cuenta #f1. El día será ingresado
+    //en el formato YYYY-MM-DD.
     @Override
     public HashTag hashtagMasUsado(LocalDate dia) {
         for (int i = 0; i < hashtags.size(); i++) {
@@ -93,12 +107,16 @@ public class SistemaTweetsImp implements SistemaTweets{
     }
 
 
+    //Top 7 cuentas con más favoritos. Para este listado se deberá retornar el nombre del
+    //usuario, junto con la cantidad de favoritos.
     @Override
     public MyQueue<User> usuariosMasFavoritos() {
 
         return null;
     }
 
+    //Cantidad de tweets con una palabra o frase específicos (que será ingresado como
+    //parámetro)
     @Override
     public int cantidadTweetsPalabra(String frase) {
         return 0;
