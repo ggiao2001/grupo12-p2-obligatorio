2f08919772180514ef64ab2047c3f4e5e2256f00
Tomás Bordaberry <tbordaberry@gmail.com>
2023-06-24 19:10:20 -0300

    Función 1,3, 4 y 5 OK.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,20 @@ import uy.edu.um.prog2.adt.Exceptions.FullHeapException;
 import uy.edu.um.prog2.adt.Exceptions.OutOfBoundsException;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Main {
     public static void main(String[] args) throws EmptyTreeException, FullHeapException, OutOfBoundsException {
+        //LocalTime start = LocalTime.now();
         CSV.getDrivers();
         CSV.getCsvInfo();
+
+        //LocalTime end = LocalTime.now();
+        //System.out.println("Tiempo de carga del CSV:" + (start.getNano() - end.getNano()) +" Nanosegundos");
+        // 876421000 Nanosegundos ~ 0.87 segundos.
+
         FrontEnd frontEnd = new FrontEnd();
         frontEnd.Menu();
     }
